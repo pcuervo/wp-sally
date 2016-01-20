de65b446f0fcae0568f08a62e1a21c8036d36b08
Raúl De Zamacona <raulppcuervo.com>
2016-01-19 21:16:57 -0600

    home image changed

--- themes/sally/footer.php
+++ themes/sally/footer.php
@@ -1,5 +1,33 @@
 		<?php if ( is_home() ) : ?></div><!-- bg-home bg-image --><?php endif; ?>
-		<a href="#">términos y condiciones</a>
 		<?php wp_footer(); ?>
+
+		<!-- /////// -->
+		<!-- Footers -->
+		<!-- /////// -->
+
+		<?php if (is_home()){ ?>
+			<footer class="[ text-center ][ width-100 ][ absolute bottom-10 ]">
+				<a class="[ color-primary ]" href="#">términos y condiciones</a>
+			</footer>
+		<?php } ?>
+
+		<?php if (is_page('pasos-explicativos') OR is_page('grabando')){ ?>
+			<footer class="[ text-center ][ width-100 ][ absolute bottom-10 ]">
+				<a class="[ color-light ]" href="#">términos y condiciones</a>
+			</footer>
+		<?php } ?>
+
+		<?php if ( is_page('ver-tutoriales') ){ ?>
+			<footer class="[ text-center ][ width-100 ][ margin-bottom--small ]">
+				<a class="[ color-primary ]" href="#">términos y condiciones</a>
+			</footer>
+		<?php } ?>
+
+		<?php if (is_page('exito') OR is_page('vobo')){ ?>
+			<footer class="[ text-center ][ width-100 ]">
+				<a class="[ color-light ]" href="#">términos y condiciones</a>
+			</footer>
+		<?php } ?>
+
 	</body>
 </html>
